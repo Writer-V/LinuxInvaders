0961dbaba47fcf8f31f33b2e9917dcac292ecd08
Vincent <vincent@tesseract.se>
2026-09-07 14:20:32 +0200

    Checkpoint before replacing game logic with minimal MonoGame template

---
+++ LinuxInvaders/LinuxInvaders.Core/Game/FaceDirection.cs
@@ -0,0 +1,11 @@
+﻿namespace LinuxInvaders.Core
+{
+    /// <summary>
+    /// Facing direction along the X axis.
+    /// </summary>
+    enum FaceDirection
+    {
+        Left = -1,
+        Right = 1,
+    }
+}
---
+++ LinuxInvaders/LinuxInvaders.Core/LinuxInvadersGame.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using LinuxInvaders.Core.Effects;
+using LinuxInvaders.Core.Localization;
+using LinuxInvaders.Core.Settings;
+using LinuxInvaders.ScreenManagers;
+using LinuxInvaders.Screens;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace LinuxInvaders.Core
+{
+    public class LinuxInvadersGame : Game
+    {
+        public LinuxInvadersGame()
+        {
+            
+        }
+
+        protected override void Initialize()
+        {
+            base.Initialize();   
+        }
+
+        protected override void LoadContent()
+        {
+            base.LoadContent();
+        }
+    }
+}
