a84906845d13945e2d6892e6cd707eaeac6aae25
Vincent <vincent@tesseract.se>
2026-09-07 14:28:33 +0200

    Completely removed target platforms except Linux and Windows. Also stripped any other content to just correspond to the base Desktop Application.

--- LinuxInvaders/LinuxInvaders.Core/LinuxInvadersGame.cs
+++ LinuxInvaders/LinuxInvaders.Core/LinuxInvadersGame.cs
@@ -1,31 +1,44 @@
-using System;
-using System.Collections.Generic;
-using System.Globalization;
-using LinuxInvaders.Core.Effects;
-using LinuxInvaders.Core.Localization;
-using LinuxInvaders.Core.Settings;
-using LinuxInvaders.ScreenManagers;
-using LinuxInvaders.Screens;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 
 namespace LinuxInvaders.Core
 {
     public class LinuxInvadersGame : Game
     {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
         public LinuxInvadersGame()
         {
-            
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
         }
 
         protected override void Initialize()
         {
-            base.Initialize();   
+            base.Initialize();
         }
 
         protected override void LoadContent()
         {
-            base.LoadContent();
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            base.Draw(gameTime);
         }
     }
 }
