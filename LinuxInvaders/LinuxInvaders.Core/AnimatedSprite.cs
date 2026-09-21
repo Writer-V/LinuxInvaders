77a07c9b0dd2ea4d6d7d7bf04c8acfeb48a55403
Writer-V <vincent@tesseract.se>
2026-09-21 13:59:54 +0200

    Final-final for V1. Onto V2 properly.

--- LinuxInvaders/LinuxInvaders.Core/AnimatedSprite.cs
+++ LinuxInvaders/LinuxInvaders.Core/AnimatedSprite.cs
@@ -2,7 +2,7 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 
-namespace LinuxInvaders.Core.AnimatedSprite
+namespace LinuxInvaders.Core
 {
 	public class AnimatedTexture
 	{
